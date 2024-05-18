2abd6afd81a936fcd4c9fcf28f8613a5cd3b0e1e
Mohammad-Ihmeid <mohammadmaher1999@yahoo.com>
2024-05-18 13:51:58 +0300

    Add error file to progect

--- lib/core/res/media_res.dart
+++ lib/core/res/media_res.dart
@@ -5,4 +5,10 @@ class MediaRes {
   static const _baseImages = 'assets/images';
   static const _baseLottie = 'assets/lottie';
   static const _baseVectors = 'assets/vectors';
+
+  //Backgroun
+  static const onBoardingBackground = '$_baseImages/on_boarding_background.png';
+
+  //Lottie
+  static const pageUnderConstruction = '$_baseLottie/under_construction.json';
 }
