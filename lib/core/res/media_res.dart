cc2518fafdf82ed4a15d7e7fc281e19076138c78
Mohammad-Ihmeid <mohammadmaher1999@yahoo.com>
2024-05-18 10:43:34 +0300

    Creating a file for storing images and icons...

---
+++ lib/core/res/media_res.dart
@@ -0,0 +1,8 @@
+class MediaRes {
+  MediaRes._();
+
+  static const _baseIcons = 'assets/icons';
+  static const _baseImages = 'assets/images';
+  static const _baseLottie = 'assets/lottie';
+  static const _baseVectors = 'assets/vectors';
+}
